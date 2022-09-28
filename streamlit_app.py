d91a7f64675aec1d632b2f3bd719d2ca39c2bebf
stpypi <37857350+stpypi@users.noreply.github.com>
2022-09-27 18:18:37 -0700

    Update streamlit_app.py
    added pandas lib and the read from the file from s3 bucket and then show it on streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Created the main python file.
 
 import streamlit 
+import pandas
 
 streamlit.title("My parents new healthy Diner")
 streamlit.header("Breakfast Menu")
@@ -12,3 +13,6 @@ streamlit.text(" 🐔 Hard-Boiled Free-Range Egg ")
 
    
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv_file('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
