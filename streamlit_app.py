f605266a74869f4b2b04c2e2c81851d8e9b09d20
stpypi <37857350+stpypi@users.noreply.github.com>
2022-09-27 18:34:57 -0700

    Update streamlit_app.py
    cleared typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick list here so they can pick the fruits they  want to include 
-steramlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
 
 #display the table on the page 
 streamlit.dataframe(my_fruit_list)
