d5c1e359cfb9a9a12f8d3479259621098b1aeb69
stpypi <37857350+stpypi@users.noreply.github.com>
2022-09-27 18:12:27 -0700

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@
 import streamlit 
 
 streamlit.title("My parents new healthy Diner")
+streamlit.header("Breakfast Menu")
+steamlit.text("Idly Sambar")
+steamlit.text("Poha")
+steamlit.text("Sandwitch")
+steamlit.text("Oat Milk")
