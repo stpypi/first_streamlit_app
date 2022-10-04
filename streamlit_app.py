ab678e8b36ddb097afab809584ab4483da1b534c
stpypi <37857350+stpypi@users.noreply.github.com>
2022-10-04 14:55:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,8 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 #streamlit.text("Hellow from Snowflake : ")
 streamlit.text(" The fruit load list contains ")
-streamlit.text(my_data_row)
+#streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
 
 
 
