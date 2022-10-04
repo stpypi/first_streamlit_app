1f793f363513b36682680673e58708db90bf954c
stpypi <37857350+stpypi@users.noreply.github.com>
2022-10-04 11:50:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,10 @@ streamlit.dataframe(fruits_to_show)
 #display the table on the page 
 streamlit.dataframe(my_fruit_list)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 streamlit.header("Fruityvice Fruit Advice!")
 #streamlit.text(fruityvice_response.json())
 # normalize the json version of the response 
