854f9e9ca9a2deeec13f49a523ad4aef3a6aa436
stpypi <37857350+stpypi@users.noreply.github.com>
2022-10-04 16:37:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,18 +31,29 @@ streamlit.dataframe(fruits_to_show)
 #display the table on the page 
 streamlit.dataframe(my_fruit_list)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+#streamlit.write('The user entered ', fruit_choice)
+
+
 streamlit.header("Fruityvice Fruit Advice!")
+
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice: 
+    streamlit.error("Please select a fruit to get information")
+    else:
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)       
+     # pandas converts the json into the data frame a tabular format to display
+     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+     streamlit.dataframe(fruityvice_normalized)
+except URLError as e: 
+  streamlit.error()
+
 #streamlit.text(fruityvice_response.json())
 # normalize the json version of the response 
-# pandas converts the json into the data frame a tabular format to display
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # and streamlit displays the data from the fruityvice_normalized variable. 
-streamlit.dataframe(fruityvice_normalized)
-streamlit.stop()
+
+# streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
