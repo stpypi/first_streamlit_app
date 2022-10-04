249343687f60ba801e3a070f364c934f083c750f
stpypi <37857350+stpypi@users.noreply.github.com>
2022-10-04 16:38:08 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice: 
     streamlit.error("Please select a fruit to get information")
-    else:
+   else:
      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)       
      # pandas converts the json into the data frame a tabular format to display
      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
